7538492cba7d9bb8b8fe8c8b4ad131ad50e243e4
48lika48 <48lika48@mail.ru>
2020-04-01 14:26:38 +0300

    feat: add change language

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+let valueENG = [
+    '`', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '-', '=', 'Backspace',
+    'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'DEL',
+    'Caps Lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'ENTER',
+    'Shift', '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '.', ',', '/', '⯅', 'Shift',
+    'Ctrl', 'Win', 'Alt', '', 'Alt', 'Ctrl', '◄', '▼', '►'
+]
+
+let valueRU = [
+    'ё', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '-', '=', 'Backspace',
+    'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'DEL',
+    'Caps Lock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'ENTER',
+    'Shift', '\\', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '⯅', 'Shift',
+    'Ctrl', 'Win', 'Alt', '', 'Alt', 'Ctrl', '◄', '▼', '►'
+]
+
 let body = document.querySelector('body');
 
 //input
@@ -12,13 +28,7 @@ keyboard.className = 'keyboard';
 body.appendChild(keyboard);
 
 
-let valueENG = [
-    '`', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '-', '=', 'Backspace',
-    'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'DEL',
-    'Caps Lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'ENTER',
-    'Shift', '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '.', ',', '/', '⯅', 'Shift',
-    'Ctrl', 'Win', 'Alt', '', 'Alt', 'Ctrl', '◄', '▼', '►'
-]
+
 
 for(let i = 0; i < valueENG.length; i ++) {
     let button = document.createElement("button");
@@ -45,3 +55,27 @@ for(let i = 0; i < valueENG.length; i ++) {
     keyboard.childNodes[63].className = 'dark';
     keyboard.childNodes[64].className = 'dark';
 
+    //change language
+    document.addEventListener('keydown', function(event) {
+        if (event.ctrlKey && event.shiftKey) {
+            let arr = document.querySelectorAll('button');
+            if (arr[44].innerHTML == 'z') {
+                
+                arr.forEach(function(currentValue, index){
+                    return currentValue.innerHTML = valueRU[index];
+                })
+            }
+            else /*(arr[44].innerHTML == 'я')*/ {
+                arr.forEach(function(currentValue, index){
+                    return currentValue.innerHTML = valueENG[index];
+                })
+                return arr;
+            }
+        }
+    }
+    
+    
+
+
+
+);
