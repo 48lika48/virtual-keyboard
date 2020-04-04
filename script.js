62856a3e398f21f65d0f3965b1689712d7089f46
48lika48 <48lika48@mail.ru>
2020-04-04 17:57:45 +0300

    feat: add caps lock activity

--- script.js
+++ script.js
@@ -22,6 +22,22 @@ let valueRU = [
     'Ctrl', 'Win', 'Alt', '', 'Alt', 'Ctrl', '◄', '▼', '►'
 ]
 
+let valueCapsENG = [
+    '`', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '-', '=', 'Backspace',
+    'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\', 'DEL',
+    'Caps Lock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '\'', 'ENTER',
+    'Shift', '\\', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '.', ',', '/', '⯅', 'Shift',
+    'Ctrl', 'Win', 'Alt', '', 'Alt', 'Ctrl', '◄', '▼', '►'
+]
+
+let valueCapsRU = [
+    'Ё', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '-', '=', 'Backspace',
+    'Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '\\', 'DEL',
+    'Caps Lock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'ENTER',
+    'Shift', '\\', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '.', '⯅', 'Shift',
+    'Ctrl', 'Win', 'Alt', '', 'Alt', 'Ctrl', '◄', '▼', '►'
+]
+
 let body = document.querySelector('body');
 
 //input
@@ -67,7 +83,7 @@ keyboard.childNodes[64].className = 'dark';
 
 //change language
 let arr = document.querySelectorAll('button');
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', (event) => {
     if (event.ctrlKey && event.shiftKey) {
         if (arr[44].innerHTML == 'z') {
             
@@ -80,6 +96,29 @@ document.addEventListener('keydown', function(event) {
                 return currentValue.innerHTML = valueENG[index];
             })
             return arr;
+        }
+    }
+
+    if (event.code == "CapsLock") {
+        if (arr[44].innerHTML == 'z') {
+            arr.forEach(function(currentValue, index){
+                return currentValue.innerHTML = valueCapsENG[index];
+            })
+        }
+        else if(arr[44].innerHTML == 'я') {
+            arr.forEach(function(currentValue, index){
+                return currentValue.innerHTML = valueCapsRU[index];
+            })
+        }
+        else if (arr[44].innerHTML == 'Z') {
+            arr.forEach(function(currentValue, index){
+                return currentValue.innerHTML = valueENG[index];
+            })
+        }
+        else /*(arr[44].innerHTML == 'Я')*/ {
+            arr.forEach(function(currentValue, index){
+                return currentValue.innerHTML = valueRU[index];
+            })
         }
     }
 });
@@ -106,7 +145,7 @@ arr.forEach(function (button) {
         else if(button.innerHTML == 'ENTER') {
             input.value = input.value + '\n';
         }
-        else if(button.innerHTML == 'Caps Lock' || button.innerHTML == 'Shift') {
+        else if(button.innerHTML == 'Shift') {
             input.value = input.value + '';
         }
         else if(button.innerHTML == 'Ctrl' || button.innerHTML == 'Win' || button.innerHTML == 'Alt') {
@@ -118,6 +157,30 @@ arr.forEach(function (button) {
         else if(button.innerHTML == 'DEL') {
             input.value = input.value.slice(1, input.value.length);
         }
+
+        else if (button.innerHTML == 'Caps Lock') {
+            if (arr[44].innerHTML == 'z') {
+                arr.forEach(function(currentValue, index){
+                    return currentValue.innerHTML = valueCapsENG[index];
+                })
+            }
+            else if(arr[44].innerHTML == 'я') {
+                arr.forEach(function(currentValue, index){
+                    return currentValue.innerHTML = valueCapsRU[index];
+                })
+            }
+            else if (arr[44].innerHTML == 'Z') {
+                arr.forEach(function(currentValue, index){
+                    return currentValue.innerHTML = valueENG[index];
+                })
+            }
+            else /*(arr[44].innerHTML == 'Я')*/ {
+                arr.forEach(function(currentValue, index){
+                    return currentValue.innerHTML = valueRU[index];
+                })
+            }
+        }
+
         else {
         input.value = input.value + button.innerHTML;
         }
